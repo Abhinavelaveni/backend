186799f1c3058eb05529cd88b70e689e41089d79
ABHI <ABHINAELAVEI@GMAIL.COM>
2025-02-26 21:02:06 +0530

    Update MongoDB connection URI in server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(bodyParser.json());
 
 // MongoDB connection
-const mongoUri = process.env.MONGO_URI || 'mongodb+srv://<admin>:<EZfIuiVpVNaJP05V>@cluster0.bzo2s.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+const mongoUri = process.env.MONGO_URI || 'mongodb+srv://abhinav:<abcd123>@cluster0.cm8iv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
 mongoose.connect(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('Connected to MongoDB'))
     .catch(err => console.error('Failed to connect to MongoDB:', err));
